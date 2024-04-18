6f498042b6cc95d63d3fdb5117764c791577e317
jxt <jiangxt21@lzu.edu.cn>
2024-04-18 10:38:41 +0800

    init

---
+++ format.py
@@ -0,0 +1,135 @@
+import re
+import os
+
+bbl_path=r"./bdma.bbl"
+bib_path=r"./bibbook.bib"
+
+if os.path.exists(bbl_path+".old"):
+    print("Read from .old bbl file ? Default Y [Y/n]:")
+    if input().upper()!="N":
+        file = open(bbl_path+".old", "r")
+        bbl_info = file.read()
+        file.close()
+    else: 
+        file = open(bbl_path, "r")
+        bbl_info = file.read()
+        file.close()
+else:
+    file = open(bbl_path, "r")
+    bbl_info = file.read()
+    file.close()
+
+file = open(bib_path, "r")
+bib_info = file.read()
+file.close()
+
+# 1. Read author in bib
+bib_dict = {}
+
+def code_authors(_authors):
+    rt = []
+    for a in _authors:
+        
+        if a.find(",")>=0:
+            gv, sur = a.split(",")
+            gv, sur = gv.strip(), sur.strip()
+            res = re.search(r'[A-Z]+',sur)
+            # print(sur,res.start(),res.end(),len(sur))
+            
+            if res.end() == len(sur):
+                out = ".".join(sur) + "." + " " + gv
+            else:
+                out = sur[0] + ". " + gv
+        else: 
+            out = a.strip()
+        rt.append(out)
+    if len(rt) == 1:
+        res=rt[0]
+    elif len(rt) == 2:
+        res = rt[0] + " and " + rt[1]
+    else:
+        res = rt[0]
+        for i in range(len(rt) - 2):
+            res += " and " + rt[i+1]
+        res += ", " + rt[-1]
+    return res
+
+bib_idx = []
+re_bib_head = r'@[^{]*'
+re_bib_author = r'author=\{[^\n]*\}'
+for match in re.finditer(re_bib_head, bib_info):
+    bib_idx += [match.start(),match.end()]
+    # print("st match start index", match.start(), "End index", match.end())
+bib_idx = bib_idx[1:]+[-1]
+
+for i in range(len(bib_idx)//2):
+    cont = bib_info[bib_idx[2*i] : bib_idx[2*i+1]]
+    # print(cont)
+    key = cont.split(",")[0][1:]
+    author = re.search(re_bib_author, cont).group()[8:-1]
+    # title = re.search(re_bib_title, cont).group()[7:-1]
+    authors = author.split("and")
+    cnt = len(authors)
+    authors = code_authors(authors)
+    # print(authors)
+    bib_dict[key] = [authors,cnt]
+    
+# exit()
+
+# 2. Refine bbl content
+new_bbl = ""
+re_bbl_head = r'\\begin\{thebibliography\}\{\d+\}'
+re_bbl_tail = r'\\end\{thebibliography\}'
+head_idx = re.search(re_bbl_head, bbl_info).end()
+tail_idx = re.search(re_bbl_tail, bbl_info).start()
+bbl_head = bbl_info[:head_idx]
+bbl_tail = bbl_info[tail_idx:]
+
+bbl_info = bbl_info[head_idx:tail_idx].strip()
+
+re_bbl_item = r'\\bibitem\{'
+bbl_idx = []
+
+
+for match in re.finditer(re_bbl_item, bbl_info):
+    bbl_idx += [match.start(),match.end()]
+# bbl_head = bbl_info[:bbl_idx[0]]
+# bbl_tail = bbl_info[bbl_idx[-1]:]
+
+bbl_idx = bbl_idx[1:] + [-1]
+
+for i in range(len(bbl_idx)//2):
+    cont = bbl_info[bbl_idx[2*i] : bbl_idx[2*i+1]].strip()
+    cont = " ".join(cont.replace("\n"," ").split())
+    key = cont.split("}")[0]
+    fixed_author, cnt = bib_dict[key]
+    if cnt <= 2:
+        author_end = cont.find(",")
+    else:
+        i = 0
+        for match in re.finditer(",", cont):
+            if i < cnt-1:
+                i += 1
+                continue
+            author_end = match.end()
+            break
+
+    res= "\\bibitem{%s} \n %s%s \n\n"%(key,fixed_author,cont[author_end:])
+    new_bbl += res
+
+new_bbl = bbl_head + "\n\n" + new_bbl + "\n\n" + bbl_tail
+bbl_info = bbl_head + "\n\n" + bbl_info + "\n\n" + bbl_tail
+# print(new_bbl)
+# print("*"*30)
+# print(bbl_info)
+
+# 3. Save olb bbl to .old one, and overwrite to bbl
+# exit()
+
+file = open(bbl_path+".old", "w")
+file.write(bbl_info)
+file.close()
+
+file = open(bbl_path, "w")
+file.write(new_bbl)
+file.close()
---
+++ readme.md
@@ -0,0 +1,36 @@
+# bibitem generation for Big Data Mining and Analytics (BDMA)
+
+<center>
+<img src='bdma_remix.png' width='80%'> 
+</center>
+
+# 1. How to use
+
+BDMA has typical ref style which have a big difference with other journal (ieee, elsiver, etc), so I made a simple script to gerneate bbl to create bibitem.
+
+Here is the step:
+
+* Paste your `\document{...}` code into [bdma.tex](bdma.tex), compile it with tex and bibtex. (pdf->bib->pdf->pdf)
+* Run [format.py](format.py) to fix the author format of bbl file.
+* Compile tex file without bibtex (pdf), copy `\bibitem` in bbl into your tex and recompile it.
+
+# 2. Note 
+
+*Note* : NOT READY TO GO. There are some points the repo CAN'T do.
+
+* Journal name abbrevation: The instruction abbr the `Behavioral Ecology and Sociobiology` into `Behav. Ecol. Sociobiol.` . However, even the [journal itself](https://link.springer.com/journal/265) HAVE NOT use this abbr. And many article in BDMA not using this abbr, so ignored.
+* Title name connection when ended with symbol: If title ended with a symbol like `"..., does it matter ?"`, the bbl file will use `?` to overwrite `,`.
+
+# 3. Detail
+
+[ieeetr-revised.bst](ieeetr-revised.bst):
+* Italic for journal name
+* Remove `"` for journal name
+
+[format.py](format.py), change author style into:
+
+* If an author have many surname, abbrevate into one: `Abrahams, Mark V` in bbl is `M. Abrahams`.
+* If an author's surname is multi-uppercase, split it: `Phelan, JC` in bbl is `J.C. Phelan`
+
+
+Also could be used for ieee article in [ieee.tex](ieee.tex)
